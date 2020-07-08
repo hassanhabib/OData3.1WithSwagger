fec5fac9490ae5ceee2d779f9a50d68fea0c9874
★ F A I S A L ★ <faisal.muhammad@live.com>
2020-07-08 17:20:21 +0200

    Generate unique Id
    Generate unique Id for WeatherForecast objects instead of 00000000-0000-0000-0000-000000000000

--- WeatherAPI2/Controllers/WeatherForecastController.cs
+++ WeatherAPI2/Controllers/WeatherForecastController.cs
@@ -35,6 +35,7 @@ namespace WeatherAPI2.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
+                Id = Guid.NewGuid().ToString(),
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
